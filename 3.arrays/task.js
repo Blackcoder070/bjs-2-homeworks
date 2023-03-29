01c46556e52347a63e5ab1417fb9d50002fe2035
Blackcoder070 <blackcoder070@gmail.com>
2023-03-29 10:34:53 +0300

    home-work-3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,7 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
@@ -10,6 +11,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter(value => value > 0).filter(value => value % 3 === 0).map(value => value * 10)
 
   return resultArr; // array
 }
